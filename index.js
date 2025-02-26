165a8e529a7a0cab3a36b119c211d813f73ad409
Kennelma <Kennelmahernandez@gmail.com>
2025-02-26 13:16:41 -0600

    Conexion funcionando

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ app.use(bp.json());
 
 // conectar a la base de datos (MYSQL)
 var mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
+    host: '142.44.161.115',
+    user: '1700PAC12025Equi3',
+    port: 3306,
     password: '1700PAC12025Equi3#49',
     database: '1700PAC12025Equi3',
     multipleStatements: true
@@ -21,12 +22,16 @@ var mysqlConnection = mysql.createConnection({
 
 // Test de conexion abase de datos
 mysqlConnection.connect((err)=>{
-    if (err){
-        console.log('Conexion exitosa');
+    if (!err){
+        console.log('Conexion Exitosa');
     } else { 
-        console.log('Error al conectar a la Db');
+        console.log('Error al conectar la base de datos', err.message);
     }
 });
+
+//IMPORTACION DE LAS RUTAS DEL MODULO PERSONAS
+const personasRoutes = require('./modulo_personas');
+app.use('/ModuloPersonas', personasRoutes); //usa esto como base de rutas
 
 // Ejecutar el server en un puerto especifico.
 app.listen(3000,()=> console.log('server Running puerto: 3000'));
