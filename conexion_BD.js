aa41aacd8c96a5e006f93ef3fd08d323946ecf3f
Kennelma <Kennelmahernandez@gmail.com>
2025-02-28 04:10:18 -0600

    Codigo depurado y corregido

---
+++ conexion_BD.js
@@ -0,0 +1,24 @@
+//Constante para el paquete de MYSQL
+const mysql = require('mysql');
+
+//Conectar a la base de datos (MYSQL)
+var mysqlConnection = mysql.createConnection({
+    host: '142.44.161.115',
+    user: '1700PAC12025Equi3',
+    port: 3306,
+    password: '1700PAC12025Equi3#49',
+    database: '1700PAC12025Equi3',
+    multipleStatements: true
+});
+
+// Test de conexion abase de datos
+mysqlConnection.connect((err)=>{
+    if (!err){
+        console.log('Conexion Exitosa');
+    } else { 
+        console.log('Error al conectar la base de datos', err.message);
+    }
+});
+
+//Se exporta la conexión a los demás archivos
+module.exports = mysqlConnection;
--- modulo_reservas.js
+++ modulo_reservas.js
@@ -1,28 +1,8 @@
-const mysql = require('mysql');
 const express = require('express');
 const router = express.Router();
+const mysqlConnection = require('./conexion_BD');
 
-// conectar a la base de datos (MYSQL)
-var mysqlConnection = mysql.createConnection({
-    host: '142.44.161.115',
-    user: '1700PAC12025Equi3',
-    port: 3306,
-    password: '1700PAC12025Equi3#49',
-    database: '1700PAC12025Equi3',
-    multipleStatements: true
-});
-
-// Test de conexion abase de datos
-mysqlConnection.connect((err)=>{
-    if (!err){
-        console.log('Conexion Exitosa');
-    } else { 
-        console.log('Error al conectar la base de datos', err.message);
-    }
-});
-
-
-//Endpoint para INSERTAR personas
+//Endpoint para INSERTAR Reservas
 router.post("/Realizar_Reserva", (req, res) => {
     const { tabla, valores } = req.body;
     const sql = "CALL INSERT_RESERVAS (?, ?)"; 
@@ -40,23 +20,6 @@ router.post("/Realizar_Reserva", (req, res) => {
         }
     });
 });
-
-
-//Endpoint para SELECCIONAR personas
-router.get("/Informacion_Reservas", (req,res) =>{
-    const { tabla } = req.query;
-    const sql = "CALL SELECT_RESERVAS (?, ?)"; 
-
-    mysqlConnection.query(sql, [tabla], (err, rows) => {
-        if (err) {
-            return res.status(500).send("Error en la consulta.");
-        }
-        res.status(200).json(rows);
-    });
-});
-
-
-
 
 
 //SE EXPORTA EL ROUTER PARA QUE SE PUEDA USAR EN EL INDEX.JS
