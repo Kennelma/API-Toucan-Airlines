89d7bac57cd9f50c95b98d5a9c2e06dc23c91bf9
AlejandroMartinez27 <alejandro.josue2725@gmail.com>
2025-03-01 23:17:24 -0600

    insert y delete modulo notificaciones

--- modulo_notificaciones.js
+++ modulo_notificaciones.js
@@ -45,56 +45,16 @@ router.post("/CrearNotificacion", (req, res) => {
 
 
 
-
-// Endpoint para seleccionar notificacion
-router.get("/Getnotificacion", (req, res) => {
-    const { cod_notificacion, pv_accion } = req.query;
-    const sql = "CALL SELECT_NOTIFICACIONES (?,?)";
-    mysqlConnection.query(sql, [pv_accion || "notificacion", cod_notificacion || 0], (err, rows, fields) => {
-        if (!err) {
-            res.status(200).json(rows[0]);
-        } else {
-            res.status(500).send("Error al seleccionar notificacion.");
-        }
-    });
-});
-
-
-
-// Endpoint para actualizar un notificacion.
-router.put("/Actualizarnotificacion/:id", (req, res) => {
-    const notificacion = req.body;
-    const notificacionId = req.params.id;
-    const sql = "CALL UPDATE_NOTIFICACIONES(?, ?, ?, ?, ?, ?)";
-    mysqlConnection.query(
-        sql,
-        [
-            notificacionId,
-            notificacion.cod_notificacion,
-            notificacion.tipo_notificacion,
-            notificacion.tipo_alerta,
-            notificacion.prioridad,
-            notificacion.mensaje,
-            notificacion.cod_reserva
-        ],
-        (err, rows, fields) => {
-            if (!err) {
-                res.status(200).send("Notificacion actualizado correctamente!");
-            } else {
-                res.status(500).send("Error al actualizar notificacion.");
-            }
-        }
-    );
-});
-
 // Endpoint para eliminar un notificacion
 router.delete("/BorrarNotificacion/:id", (req, res) => {
-    const cod_notificacion = req.params.cod_notificacion;
-    const sql = "CALL DELETE_NOTIFICACIONES (?);";
-    mysqlConnection.query(sql, [cod_notificacion], (err, rows, fields) => {
+    const cod_notificacion = req.params.id;
+    const sql = "CALL DELETE_NOTIFICACIONES (?)";
+
+    mysqlConnection.query(sql, [cod_notificacion], (err, rows) => {
         if (!err) {
             res.status(200).send(`Notificacion con ID ${cod_notificacion} eliminado correctamente!`);
         } else {
+            console.error("error al eliminar reporte:", err);
             res.status(500).send("Error al eliminar Notificacion.");
         }
     });
